a778460565a32640eec6ea4f9841ff71189ece3b
Sávio André <savioa@834@gmail.com>
2021-10-28 18:45:24 -0300

    updates, bugs fixes

---
+++ cam.js
@@ -0,0 +1,11 @@
+/*function startVideo() {
+
+    navigator.mediaDevices.getUserMedia({video:true}).then(stream=>{
+
+        const videoElement = document.querySelector('#video')
+        videoElement.scrObject = stream
+
+    }).catch(error=>{alert('Câmera não capturada')})
+}
+
+window.addEventListener("DOMContentLoaded", startVideo)*/
--- script.js
+++ script.js
@@ -5,17 +5,46 @@ const Bar = {
     close() {
         document.querySelector(".options").classList.remove("active");
       },
-} 
+}
 
-const People = {
-    open() {
-      document.querySelector(".participants").classList.add("active");
-    },
-    close() {
-        document.querySelector(".participants").classList.remove("active");
-      },
-} 
+const AUDIO = document.querySelector('.CAD')
+const VIDEO = document.querySelector('.IVI')
+const SHARE_SCREEN = document.querySelector('.CTE')
+const PARTICIPANTS = document.querySelector('.PAR')
+const MORE = document.querySelector('.MOR')
+// ADD
+AUDIO.addEventListener('click', () => {
+  document.querySelector('.conect').classList.add('on')
+})
 
+VIDEO.addEventListener('click', () => {
+  document.querySelector('.video_cast').classList.add('on')
+})
+
+SHARE_SCREEN.addEventListener('click', () => {
+  document.querySelector('.share').classList.add('on')
+})
+
+PARTICIPANTS.addEventListener('click', () => {
+  document.querySelector('.participants').classList.add('on')
+})
+
+// REMOVE
+document.querySelector('.participants .close').addEventListener('click', () => {
+  document.querySelector('.participants').classList.remove('on')
+})
+
+document.querySelector('.conect .close').addEventListener('click', () => {
+  document.querySelector('.conect').classList.remove('on')
+})
+
+document.querySelector('.close').addEventListener('click', () => {
+  document.querySelector('.participants').classList.remove('on')
+})
+
+document.querySelector('.share .close').addEventListener('click', () => {
+  document.querySelector('.share').classList.remove('on')
+})
 
 // Get Name
 // let getName = prompt("Coloque Seu nome")
@@ -64,3 +93,18 @@ function userName() {
   //    active_bar
 
   //  }
+
+function start(ID) {
+  const shield = document.querySelector(ID);
+  if(shield) {
+    shield.classList.add('active');
+    shield.addEventListener('click', (e) => {
+      if(e.target.id == ID || e.target.className == '') {
+        shield.classList.remove('active')
+      }
+    });
+  }
+}
+
+const button = document.querySelector('.shield');
+button.addEventListener('click', () => ID(''))
